ba144c2aa2765ed802b49dbcab868abc944bd1d9
Mehak <mehakmattoo7@gmail.com>
2025-01-30 18:02:14 +0530

    minor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import FileUpload from "./components/FileUpload";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import OneWayTicket from "./components/OneWayTicket";
 import TwoWayTicket from "./components/TwoWayTicket";
-import Testing from "./components/Testing";
+
 
 function App() {
   return (
